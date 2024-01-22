c44ff8214879659cf599723f8803e4b9a7196e31
Elizaveta Khanan <154255907+Lizahanan@users.noreply.github.com>
2024-01-23 02:08:10 +0300

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -11,15 +11,16 @@ public class GameOfLife {
 		String fileName = args[0];
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
-		//// test1(fileName);
-		//// test2(fileName);
-		//// test3(fileName, 3);
-		//// play(fileName);
+		////test1(fileName);
+		////test2(fileName);
+		////test3(fileName, 3);
+		play(fileName);
 	}
 
 	// Reads the data file and prints the initial board.
 	private static void test1(String fileName) {
 		int[][] board = read(fileName);
+		//System.out.println(Arrays.deepToString(board));
 		print(board);
 	}
 
@@ -27,6 +28,15 @@ public class GameOfLife {
 	// the count and cellValue functions.
 	private static void test2(String fileName) {
 		int[][] board = read(fileName);
+		print(board);
+		for(int i=1;i<board.length-1;i++){
+			for(int j=1;j<board[i].length-1;j++){
+				int cellValue = cellValue(board, i, j);
+				int count = count(board, i, j);
+				System.out.println("for Position I="+i+"and J="+j+" Count is "+count+
+				" and CellValue is "+cellValue);
+			}
+		}
 		//// Write here code that tests that the count and cellValue functions
 		//// are working properly, and returning the correct values.
 	}
@@ -64,7 +74,30 @@ public class GameOfLife {
 		int cols = Integer.parseInt(in.readLine());
 		int[][] board = new int[rows + 2][cols + 2];
 		//// Replace the following statement with your code.
-		return null;
+		for(int i =1; i<1+rows;i++)
+		{
+			String line = in.readLine();
+			if(!line.isEmpty()){
+				int dotCount=0;
+				int xCount = 0;
+				for(int j = 0; j < line.length(); j++) {
+					char c = line.charAt(j);
+					if(c == '.'){
+						dotCount++;			
+					}
+					else if(c == 'x'){
+						xCount++;
+					}
+				  }
+				  for(int j=1+dotCount; j<1+dotCount+xCount;j++){
+					board[i][j] = 1;
+				  }
+			}
+			
+
+		}
+		
+		return board;
 	}
 
 	// Creates a new board from the given board, using the rules of the game.
@@ -72,7 +105,13 @@ public class GameOfLife {
 	// cell in the new board. Returns the new board.
 	public static int[][] evolve(int[][] board) {
 		//// Replace the following statement with your code.
-		return null;
+		int[][] newBoard = new int[board.length][board[0].length];
+		for(int i=1;i<board.length-1;i++){
+			for(int j=1;j<board[i].length-1;j++){
+				newBoard[i][j]=cellValue(board, i, j);
+			}
+		}
+		return newBoard;
 	}
 
 	// Returns the value that cell (i,j) should have in the next generation.
@@ -86,7 +125,21 @@ public class GameOfLife {
 	// Uses the count(board,i,j) function to count the number of alive neighbors.
 	public static int cellValue(int[][] board, int i, int j) {
 		//// Replace the following statement with your code.
-		return 0;
+		int cellVal = board[i][j];
+		int aliveCount = count(board, i, j);
+		//alive
+		if(cellVal == 1){
+			if(aliveCount == 2 || aliveCount ==3){
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		}
+		else if(aliveCount==3){
+			return 1;
+		}
+		return cellVal;
 	}
 
 	// Counts and returns the number of living neighbors of the given cell
@@ -95,12 +148,25 @@ public class GameOfLife {
 	// Assumes that j is at least 1 and at most the number of columns in the board - 1. 
 	public static int count(int[][] board, int i, int j) {
 		//// Replace the following statement with your code.
-		return 0;
+		int count = 0;
+		for(int k = -1; k<=1;k++){
+			for(int m = -1; m<=1;m++){
+				count+=board[i+k][j+m];
+			}
+		}
+		count-= board[i][j];
+		return count;
 	}
 
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
 		//// Write your code here.
+		for (int i = 1; i < arr.length-1; i++) {
+			for (int j = 1; j < arr[i].length-1; j++) {
+				System.out.printf("%d ", arr[i][j]); 
+			}
+			System.out.println(); 
+		}
 	}
 
     // Displays the board. Living and dead cells are represented by black and white squares, respectively.
@@ -136,3 +202,4 @@ public class GameOfLife {
 		StdDraw.pause(100); 
 	}
 }
+
